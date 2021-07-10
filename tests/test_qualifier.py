e08b6e263be92f06e3a0b037b055a8b6462fa97d
sangramsinghg <sangramsinghg@yahoo.com>
2021-07-10 14:46:29 -0700

    Added the test filter validation code

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -8,10 +8,11 @@ from qualifier.utils import fileio
 from qualifier.utils import calculators
 
 # Import Filters
-from qualifier.filters import credit_score
-from qualifier.filters import debt_to_income
-from qualifier.filters import loan_to_value
-from qualifier.filters import max_loan_size
+from qualifier.filters.max_loan_size import filter_max_loan_size
+from qualifier.filters.credit_score import filter_credit_score
+from qualifier.filters.debt_to_income import filter_debt_to_income
+from qualifier.filters.loan_to_value import filter_loan_to_value
+
 
 def test_save_csv():
     qualifying_loans_path = "./data/output/qualifying_loans.csv"
@@ -40,5 +41,30 @@ def test_filters():
 
     loan_to_value_ratio = 0.84
 
-    # @TODO: Test the new save_csv code!
-    # YOUR CODE HERE!
+    header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
+
+    loans_initial_path = './tests/output_validation/loans_initial.csv'
+    fileio.save_csv(loans_initial_path, header, bank_data)
+
+    bank_data_filtered = filter_max_loan_size(loan, bank_data)
+    assert len(bank_data_filtered) == 18
+    loans_max_loan_path = './tests/output_validation/loans_max_loan.csv'
+    fileio.save_csv(loans_max_loan_path, header, bank_data_filtered)
+
+    bank_data_filtered = filter_credit_score(current_credit_score, bank_data_filtered)
+    assert len(bank_data_filtered) == 9
+    loans_credit_score_path = './tests/output_validation/loans_credit_score.csv'
+    fileio.save_csv(loans_credit_score_path, header, bank_data_filtered)
+
+    bank_data_filtered = filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
+    assert len(bank_data_filtered) == 8
+    loans_debt_ratio_path = './tests/output_validation/loans_debt_ratio.csv'
+    fileio.save_csv(loans_debt_ratio_path, header, bank_data_filtered)
+
+    bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
+    assert len(bank_data_filtered) == 6
+
+    # Test the save csv file functionality 
+    qualifying_loans_path = './tests/output_validation/qualifying_loans_final.csv'
+    fileio.save_csv(qualifying_loans_path, header, bank_data_filtered)
+    assert Path(qualifying_loans_path).exists() == True
