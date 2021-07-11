bc0b9146baee10ab17db39df831886a7413d7ae8
sangramsinghg <sangramsinghg@yahoo.com>
2021-07-10 19:23:08 -0700

    improve error handling for inputs. Add support to debug the application.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import sys
 import fire
 import questionary
 from pathlib import Path
+import signal
 
 from qualifier.utils.fileio import load_csv
 from qualifier.utils.fileio import save_csv
@@ -24,11 +25,18 @@ from qualifier.filters.credit_score import filter_credit_score
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
 
+# Global to control debug output
+debug = False
+
 def check_csvpath(csvpath):
-    if csvpath and csvpath.lower().endswith(".csv") and len(csvpath) >= 5:
+    if csvpath == None:
+        sys.exit("Ctrl+C detected. Exiting")
+    elif csvpath and csvpath.lower().endswith(".csv") and len(csvpath) >= 5:
         return True
     else:
-        sys.exit(f"Incorrect or incomplete csv path '{csvpath}' entered - should end in .csv and should be greater than 4 characters")
+        print(f"Incorrect or incomplete csv path '{csvpath}' entered - should end in .csv and should be greater than 4 characters")
+        print("Press Ctrl+C to exit")
+        return False
 
 
 def load_bank_data():
@@ -47,8 +55,29 @@ def load_bank_data():
         csvpath = Path(csvpath)
         if not csvpath.exists():
             sys.exit(f"Oops! Can't find this path: {csvpath}")
-
         return load_csv(csvpath)
+    else:
+        load_bank_data()
+
+def get_user_applicant_info(query_text, info_type):
+    """Prompt dialog to get the applicant's financial information.
+
+    Input:
+        query_text: the text that is prompted to the user
+        info_type: int, float, etc. that describe the type of the expected query
+    Returns:
+        Returns the applicant's financial information only if it is valid as per type.
+    """
+    while True:
+        try:
+            info = questionary.text(f"What's your {query_text}?").ask()
+            if info == None:
+                sys.exit("Ctrl+C detected. Exiting the program")
+            info = info_type(info)
+            break
+        except Exception as exception:
+            print(f"invalid {query_text}. Try again or Ctrl+C to exit.")
+    return info
 
 def get_applicant_info():
     """Prompt dialog to get the applicant's financial information.
@@ -57,7 +86,7 @@ def get_applicant_info():
         Returns the applicant's financial information.
     """
 
-    ''' Sample Input
+    ''' Sample Applicant Input
 
         credit score: 600
         monthly debt: 10000
@@ -66,17 +95,18 @@ def get_applicant_info():
         home value: 200000
     '''
 
-    credit_score = questionary.text("What's your credit score?").ask()
-    debt = questionary.text("What's your current amount of monthly debt?").ask()
-    income = questionary.text("What's your total monthly income?").ask()
-    loan_amount = questionary.text("What's your desired loan amount?").ask()
-    home_value = questionary.text("What's your home value?").ask()
+    credit_score = get_user_applicant_info("credit score", int)
+    debt = get_user_applicant_info("current amount of monthly debt", float)
+    income = get_user_applicant_info("total monthly income", float)
+    loan_amount = get_user_applicant_info("desired loan amount", float)
+    home_value = get_user_applicant_info("home value", float)
 
-    credit_score = int(credit_score)
-    debt = float(debt)
-    income = float(income)
-    loan_amount = float(loan_amount)
-    home_value = float(home_value)
+    if debug == True:
+        print(f"Credit score : {credit_score}")
+        print(f"Debt         : {debt}")
+        print(f"Income       : {income}")
+        print(f"Loan Amount  : {loan_amount}")
+        print(f"Home Value   : {home_value}")
 
     return credit_score, debt, income, loan_amount, home_value
 
@@ -113,36 +143,65 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     # Run qualification filters
     bank_data_filtered = filter_max_loan_size(loan, bank_data)
-    print(f"TODO Found {len(bank_data_filtered)} qualifying loans based on max loan size")
+    if debug == True:
+        print(f"Found {len(bank_data_filtered)} qualifying loans based on max loan size filter")
+
     bank_data_filtered = filter_credit_score(credit_score, bank_data_filtered)
-    print(f"TODO Found {len(bank_data_filtered)} qualifying loans based on credit score")
+    if debug == True:
+        print(f"Found {len(bank_data_filtered)} qualifying loans based on credit score filter")
+
     bank_data_filtered = filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
-    print(f"TODO Found {len(bank_data_filtered)} qualifying loans based on debt to income")
+    if debug == True:
+        print(f"Found {len(bank_data_filtered)} qualifying loans based on debt to income filter")
+
     bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
+    if debug == True:
+        print(f"Found {len(bank_data_filtered)} qualifying loans based on loan to value filter")
+
     print(f"Found {len(bank_data_filtered)} qualifying loans")
 
     return bank_data_filtered
 
 
-def save_qualifying_loans(qualifying_loans):
+def save_qualifying_loans(qualifying_loans, prompt = True):
     """Saves the qualifying loans to a CSV file.
 
     Args:
         qualifying_loans (list of lists): The qualifying bank loans.
     """
-    save_the_csv = questionary.confirm("Do you want to save the loans output to a csv file?").ask()
+    if prompt == True:
+        save_the_csv = questionary.confirm("Do you want to save the loans output to a csv file?").ask()
+    else:
+        save_the_csv = True
+
     if save_the_csv == True:
         csvpath = questionary.text("Please provide the csv path (ending in .csv) where you want to save the output:").ask()
         if check_csvpath(csvpath):
             header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
             save_csv(csvpath, header, qualifying_loans)
+        else:
+            save_qualifying_loans(qualifying_loans, False)
     else:
         print("As desired, we are not saving the output in a csv file")
 
 
 
-def run():
+def run(verbose = False, help = False, v = False, h = False):
     """The main function for running the script."""
+
+    # Print the help options
+    if help == True or h == True:
+        print("python app.py --verbose : for debug mode")
+        print("python app.py --v       : for debug mode")
+        print("python app.py --help    : for help options")
+        print("python app.py --h       : for help options")
+        sys.exit()
+
+    # Set the debugging mode based on the verbose or v option
+    if verbose == True or v == True:
+        global debug
+        debug = True
+        print(f"Verbose mode: setting debug to {debug}")
 
     # Load the latest Bank data
     bank_data = load_bank_data()
